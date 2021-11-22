3581b2ab6f6e7b914a7517d8fec31abd07ee913f
CITIGO\datnt1 <dat.nt4@citigo.com.vn>
2021-11-22 14:46:51 +0700

    Update Buoc_2

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Todo from "./Todo";
+import TodoList from "./TodoList";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,74 +8,10 @@ class App extends React.Component {
     this.state = { taskName: "", tasks: [] };
   }
 
-  myTaskChangeHandler = (event) => {
-    this.setState({ taskName: event.target.value });
-    // console.log("myTaskChangeHandler", event.target.value);
-  };
-
-  addTask = () => {
-    if (this.state.taskName === "") {
-      return;
-    }
-    const id = this.state.tasks.length;
-    const name = this.state.taskName;
-    this.state.tasks.push({ id, name });
-    this.setState({ taskName: "" });
-  };
-
-  deleteTask = (id) => {
-    // console.log("list", this.state.tasks);
-    console.log("deleteTask", id);
-    const tasks = this.state.tasks.filter((task) => task.id !== id);
-    console.log("Sau khi xoa", tasks);
-    this.setState({ tasks });
-  };
-
-  completeTask = (id) => {
-    console.log("completeTask", id);
-    const tasks = this.state.tasks;
-    tasks.forEach((task) => {
-      if (task.id === id) {
-        task.done = true;
-      }
-    });
-    console.log("Task completed", tasks);
-    this.setState({ tasks });
-  };
-
   render() {
     return (
       <div className="App">
-        <br />
-        To do List
-        <br />
-        <div className="aligned">
-          <img
-            src="./images/32379_add_plus_icon.png"
-            alt="Add Task"
-            width="50"
-            style={{ cursor: "pointer" }}
-            onClick={() => this.addTask()}
-          />
-          <input
-            type="text"
-            value={this.state.taskName}
-            onChange={this.myTaskChangeHandler}
-          />
-        </div>
-        <ul style={{ paddingLeft: "5px" }}>
-          {this.state.tasks.map((value, index) => {
-            return (
-              <Todo
-                key={index}
-                id={value.id}
-                value={value.name}
-                deleteTask={this.deleteTask}
-                completeTask={this.completeTask}
-              />
-            );
-          })}
-        </ul>
+        <TodoList />
       </div>
     );
   }
--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -4,38 +4,53 @@ import "./Todo.css";
 class Todo extends React.Component {
   constructor(props) {
     super(props);
-    console.log("props", props);
+    this.state = { id: props.id, value: props.value, done: props.done };
+    // console.log("props", props);
     // this.state = { taskName: "" };
   }
 
-  deleteTask = (e) => {
-    console.log("deleteTask from Todo", e);
+  completeTask = () => {
+    console.log("Todo completeTask", this.props.id);
+    this.setState({ done: true });
+    this.props.completeTask(this.props.id);
   };
 
   render() {
-    return (
-      <div className="Todo aligned">
+    let checkDone = "";
+    if (!this.state.done) {
+      checkDone = (
         <img
           src="./images/1398911_correct_mark_success_tick_valid_icon.png"
           alt="Hoàn thành"
           width="30"
           style={{ cursor: "pointer" }}
-          title="Click to complete comfirmation"
-          onClick={() => this.props.completeTask(this.props.id)}
+          onClick={() => this.completeTask(this.props.id)}
         />
+      );
+    }
+    return (
+      <div className="Todo aligned">
+        {checkDone}
         &nbsp;
         <img
+          className={this.state.done ? "noCheckDone" : ""}
           src="./images/71063_empty_trash_icon.png"
           alt="Xóa"
           width="30"
           style={{ cursor: "pointer" }}
-          title="Click to delete comfirmation"
           onClick={() => this.props.deleteTask(this.props.id)}
         />
         &nbsp; &nbsp;
-        <span>{this.props.value}</span>
+        <span className={this.state.done ? "done" : ""}>
+          {/* Tên class = "done" */}
+          {this.state.value}
+        </span>
       </div>
     );
   }
 }
 export default Todo;
+
+// Note:
+// - State: Dữ liệu nội tại của Component hiện tại.
+// - Props: Dữ liệu truyền từ ngoài vào, từ Element cha.
